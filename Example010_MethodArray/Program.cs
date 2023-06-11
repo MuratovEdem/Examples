9c68299ce7760cdccde92d467cc3a8eb7c8e9cd9
Edem <edem.muratov01@mail.ru>
2023-06-11 18:24:28 +0300

    Example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 14, 32, 53, 47, 56, 65, 73, 28 };
+
+int n = array.Length;
+int find = 47;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find);
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; //index = index + 1;
+}
